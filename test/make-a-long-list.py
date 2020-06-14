43308185e3a6415f28a59d704716ded97c8f525c
Tod <todlarson@gmail.com>
2020-06-14 15:28:53 -0400

    --amend

--- test/make-a-long-list.py
+++ test/make-a-long-list.py
@@ -1,4 +1,18 @@
 from random import randrange
+from jinja2 import Template
+
+notvalidatingflag = False
+
+data = '''-
+    what: prod
+    address: {{address}}
+    description: {{description}}
+    location: {{az|upper}}
+    role: vlan
+    camera_default_gateway: {{gw}}
+    camera_ip_range: {{ipstart}}-{{ipend}}'''
+tm=Template(data)
+
 NumberPOEperAZ=randrange(3,10)
 for az in ['dca50','dca51','dca54','apa60','apa61','apa62','lck50','lck51','lck52']:
     switchnum = 100
@@ -16,8 +30,11 @@ for az in ['dca50','dca51','dca54','apa60','apa61','apa62','lck50','lck51','lck5
                 else: switchnum = switchnum + 1
                 ipstart = octet4 + 5
                 ipend = ipstart + 23
-                gw = octet4 + 1
+                gw = '192.168.' + str(octet3) + '.' + str(octet4 + 1)
                 address = '192.168.' + str(octet3) + '.' + str(octet4) + '/27'
-                print ('Switch {3}-{4}-ws-sc-acc-sw{2} Subnet: {5} gw:{6} Camera IP:{7}-{8}'.format(octet3,octet4,switchnum,az,aznum,address,gw,ipstart,ipend))
+                description = 'Access switch {3}-{4}-ws-sc-acc-sw{2} camera subnet'.format(octet3,octet4,switchnum,az,aznum,address,gw,ipstart,ipend)
+                msg = tm.render(az=az,address=address,description=description,gw=gw,ipstart=ipstart,ipend=ipend)
+                msgvalidation = '{3}-{4}-ws-sc-acc-sw{2} {5:20} gw:{6:17} {7:3}-{8:3}'.format(octet3,octet4,switchnum,az,aznum,address,gw,ipstart,ipend)
+                print(msg) if notvalidatingflag else print(msgvalidation)
                 if switchnum == 191: switchnum = 100; fiberflag = False
         octet3 = octet3 + 1
