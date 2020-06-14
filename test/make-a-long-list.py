c0b476a75ef95028b69e15142201faf2d8079eb2
Tod <todlarson@gmail.com>
2020-06-14 15:36:01 -0400

    validation flag to switch between easy to read and yaml ouput

--- test/make-a-long-list.py
+++ test/make-a-long-list.py
@@ -1,7 +1,7 @@
 from random import randrange
 from jinja2 import Template
 
-notvalidatingflag = False
+notvalidatingflag = True
 
 data = '''-
     what: prod
@@ -10,7 +10,7 @@ data = '''-
     location: {{az|upper}}
     role: vlan
     camera_default_gateway: {{gw}}
-    camera_ip_range: {{ipstart}}-{{ipend}}'''
+    camera_ip_range: {{cameraiprange}}'''
 tm=Template(data)
 
 NumberPOEperAZ=randrange(3,10)
@@ -30,11 +30,12 @@ for az in ['dca50','dca51','dca54','apa60','apa61','apa62','lck50','lck51','lck5
                 else: switchnum = switchnum + 1
                 ipstart = octet4 + 5
                 ipend = ipstart + 23
+                cameraiprange = '192.168.' + str(octet3) + '.' + str(ipstart) + '-' + str(ipend)
                 gw = '192.168.' + str(octet3) + '.' + str(octet4 + 1)
                 address = '192.168.' + str(octet3) + '.' + str(octet4) + '/27'
-                description = 'Access switch {3}-{4}-ws-sc-acc-sw{2} camera subnet'.format(octet3,octet4,switchnum,az,aznum,address,gw,ipstart,ipend)
-                msg = tm.render(az=az,address=address,description=description,gw=gw,ipstart=ipstart,ipend=ipend)
-                msgvalidation = '{3}-{4}-ws-sc-acc-sw{2} {5:20} gw:{6:17} {7:3}-{8:3}'.format(octet3,octet4,switchnum,az,aznum,address,gw,ipstart,ipend)
+                description = 'Access switch {3}-{4}-ws-sc-acc-sw{2} camera subnet'.format(octet3,octet4,switchnum,az,aznum,address,gw)
+                msg = tm.render(az=az,address=address,description=description,gw=gw,cameraiprange=cameraiprange)
+                msgvalidation = '{3}-{4}-ws-sc-acc-sw{2} subnet:{5:20} gw:{6:17} CameraIPs:{7}'.format(octet3,octet4,switchnum,az,aznum,address,gw,cameraiprange)
                 print(msg) if notvalidatingflag else print(msgvalidation)
                 if switchnum == 191: switchnum = 100; fiberflag = False
         octet3 = octet3 + 1
