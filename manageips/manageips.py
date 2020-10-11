bb0a05bf8dc3a1b41a2dcc44663c93c51df31adf
Tod <todlarson@gmail.com>
2020-10-11 16:14:38 -0400

    add, delete, get, list features

--- manageips/manageips.py
+++ manageips/manageips.py
@@ -1,23 +1,93 @@
+#!/usr/local/bin/python3
 import yaml
+import sys
 import ipaddress
 
-with open(r'myipam.yaml') as file:
+def add_host(myipam, newhost):
+    if newhost not in myipam['console']['hosts']:
+        myipam['console']['hosts'][newhost] = {}
+        return myipam
+    else:
+        print("Error:", newhost,"already exists")
+        exit()
+
+def delete_host(myipam, newhost):
+    if newhost in myipam['console']['hosts']:
+        del myipam['console']['hosts'][newhost]
+        return myipam
+    else:
+        print("Error:", newhost," does not exist")
+        exit()
+
+def add_address_to_host(myipam, host, lo0):
+    if lo0 not in myipam['console']['hosts'][host]:
+        myipam['console']['hosts'][host]['loopback0'] = []
+        myipam['console']['hosts'][host]['loopback0'] = lo0
+    else:
+        print("Error:", lo0,"already exists for ", host)
+
+def find_new_loopback(myipam, new_address):
+    tracking_list = []
+    new_address = ''
+    for x in myipam['console']['hosts']:
+        tracking_list.append(ipaddress.ip_address(myipam['console']['hosts'][x]['loopback0']))
+    for candidate in list(ipaddress.ip_network(str(myipam['console']['aggregate']['dc']['network']))):
+        if candidate not in tracking_list:
+            new_address = candidate
+            return new_address
+
+def get_host_address(myipam, newhost):
+    if newhost in myipam['console']['hosts']:
+        print(newhost, myipam['console']['hosts'][newhost]['loopback0'])
+    else:
+        print(newhost, "not found.")
+        exit()
+
+def get_host_address_all(myipam):
+    for host in myipam['console']['hosts']:
+        print(host, myipam['console']['hosts'][host]['loopback0'])
+
+def write_to_file(myipam, filename):
+    with open(filename, 'w') as file:
+        documents = yaml.dump(myipam, file)   
+
+#Process args
+input_error_msg = "Error -  usage: manageips.py --[add|delete|get|list] [--hostname <hostname>]"
+if len(sys.argv) == 2 and sys.argv[1] == "--list":
+    action = str(sys.argv[1])
+elif len(sys.argv) == 2 and sys.argv[1] != "--list":
+    print(input_error_msg)
+    exit()
+elif len(sys.argv) != 4 or sys.argv[2] != "--hostname":
+    print(input_error_msg)
+    exit()
+else:
+    action = str(sys.argv[1])
+    newhost = str(sys.argv[3])
+
+# Save the entire file into a dictionary
+myipam = []
+filename = "./myipam.yaml"
+with open(filename, "r") as file:
     # The FullLoader parameter handles the conversion from YAML
     # scalar values to Python the dictionary format
-    myipam_list = yaml.load(file, Loader=yaml.FullLoader)
+    myipam = yaml.load(file, Loader=yaml.FullLoader)
 
-# Print all the aggregates
-# for key,value in myipam_list.items():
-#     if value['type'] == 'aggregate':
-#         print(value['desc'])
-#         for x in value['address']:
-#             print("    ", x)
-
-# List all the host addresses availabe in the loopback aggregate
-for x in myipam_list['aggregate_loopbacks']['address']:
-    for z in ipaddress.ip_network(x):
-        for a in myipam_list['allocated_loopbacks']['address']: 
-            if z in ipaddress.ip_network(a):            
-                print("NOT", z)
-            else:
-                print("available", z)
+if action == '--add':
+   new_address = ''
+   new_address = find_new_loopback(myipam, new_address)
+   add_host(myipam, newhost)
+   add_address_to_host(myipam, newhost, str(new_address))
+   write_to_file(myipam, filename)
+   get_host_address(myipam, newhost)
+elif action == '--delete':
+   delete_host(myipam, newhost)
+   write_to_file(myipam, filename)
+   print(newhost, "deleted.")
+elif action == '--get':
+   get_host_address(myipam, newhost)
+elif action == '--list':
+   get_host_address_all(myipam)
+else:
+    print(input_error_msg)
+    exit()
