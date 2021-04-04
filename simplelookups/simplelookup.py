51966bf2f1cdd780d150d39d80e8c47ff2886d99
Tod Larson <todlarson@gmail.com>
2021-04-04 09:37:05 -0400

    ready enough

--- simplelookups/simplelookup.py
+++ simplelookups/simplelookup.py
@@ -1,7 +1,26 @@
 import csv
+import sys
+
+#print('Number of arguments:', len(sys.argv), 'arguments.')
+#print('Argument List:', str(sys.argv))
 
 with open('/Users/todlarson/workspace/networking/simplelookups/data.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-print(data)
+if len(sys.argv) == 1:
+    for row in data:
+        print("{} - My command {} second command {}".format(row[0],row[1],row[2]))
+        print("{} - My command {} second command {}".format(row[0],row[4],row[5]))
+elif len(sys.argv) == 2:
+    found = False
+    for row in data:
+        if row[0] == sys.argv[1]:
+            found = True
+            print(row[0])
+            print("My command {} second command {}".format(row[1],row[2]))
+            print("My command {} second command {}".format(row[4],row[5]))
+    if found == False:
+        print("Input argument {} not found".format(sys.argv[1]))
+else:
+    print("Error - bad input arguments")
