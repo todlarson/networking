e41df440d0b27eb0114e604710b17ad2dad1c03c
Tod Larson <todlarson@gmail.com>
2021-04-04 09:08:27 -0400

    secn

--- simplelookups/simplelookup.py
+++ simplelookups/simplelookup.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('data.csv', newline='') as f:
+with open('/Users/todlarson/workspace/networking/simplelookups/data.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
